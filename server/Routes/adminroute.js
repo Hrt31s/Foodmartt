baee049c37ec8c02afbf000dc40c9d3bf1a3d8ab
Hrt31s <harshithector18@gmail.com>
2025-07-25 15:22:27 +0530

    first commit

---
+++ server/Routes/adminroute.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+const path = require("path");
+const multer = require("multer");
+
+const upload = require("../Middlewares/upload.js");
+const jwt = require("jsonwebtoken");
+const {
+  loginadminController,
+  registeruserController
+} = require("../Controllers/admincontroller.js");
+const verifyToken = require("../Middlewares/verifytoken.js");
+const fs = require("fs");
+const router = Router();
+
+router.post("/registeradmin", registeruserController);
+router.get("/alladmin", alluserController);
+router.post("/loginadmin", loginadminController);
+
+
+
+module.exports = router;
